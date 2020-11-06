042a6022ce36743f49558b1d13be5e4f010ca7d6
Tallal Javed <Tallal.javed@outlook.com>
2020-11-06 15:31:33 +0500

    Updated scrappers

--- scrappers/settings.py
+++ scrappers/settings.py
@@ -62,9 +62,12 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'factcheckers.pipelines.FactcheckersPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    #'factcheckers.pipelines.FactcheckersPipeline': 300,
+    'scrapy.pipelines.images.ImagesPipeline': 1,
+}
+
+IMAGES_STORE = '/Users/tallaljaved/Code/factcheckers/images'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
--- scrappers/spiders/AFPPakistan.py
+++ scrappers/spiders/AFPPakistan.py
@@ -1,10 +1,12 @@
 import scrapy
+import requests
+import mimetypes
 
 class QuotesSpider(scrapy.Spider):
     name = "AFPPaksitan"
 
     def start_requests(self):
-        urls = ['https://factcheck.afp.com/afp-pakistan']
+        urls = ['https://factcheck.afp.com/afp-pakistan?page=0']
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
@@ -12,11 +14,19 @@ class QuotesSpider(scrapy.Spider):
         urls = response.xpath('//div[@class="card"]//a/@href').extract()
         for url in urls:
             url = response.urljoin(url)
-            #print(url)
             yield scrapy.Request(url=url, callback=self.parseArticle)
+            # get new page url and extract images
+        # scrap next url
+        currentPage = int(response.url.split('=')[1])
+        if currentPage == 24:
+            print('DONEEEE DONEEE')
+            return
+        newUrl = response.url.split('=')[0]+"="+str(currentPage+1)
+        yield scrapy.Request(url=newUrl, callback=self.parse)
+
 
     def parseArticle(self, response):
         images = response.xpath('//div[@id="wysiwyg"]/img/@src').extract()
         for img in images:
-            img = response.urljoin(img)
-            print(img)
+            imgUrl = response.urljoin(img)
+            yield {'image_urls': [imgUrl]}
